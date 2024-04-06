7a9edf24676d52483f7f87479a02fb5a1e54f9e3
Hilda Zaqya <hildazaqyaelnaz@gmail.com>
2024-04-06 21:59:22 +0700

    add icon socmed done

--- kopstud-react/src/components/Menu.jsx
+++ kopstud-react/src/components/Menu.jsx
@@ -1,90 +1,90 @@
-// import React from "react";
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import { Navigation } from 'swiper';
-// import 'swiper/css';
+import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+import 'swiper/css';
 
-// class Menu extends React.Component {
-//     render() {
-//         const menuItem = [
-//             {
-//                 imageSrc: "src/img/dcream.jpg",
-//                 title: "D'cream",
-//                 description: "Drink with a combination of coffee and fresh milk to lift your mood."
-//             },
-//             {
-//                 imageSrc: "src/img/greentea.jpg",
-//                 title: "Green tea",
-//                 description: "Drink with a mixture of sweet green tea to refresh your day."
-//             },
-//             {
-//                 imageSrc: "src/img/thaitea.jpg",
-//                 title: "Thai tea",
-//                 description: "Drink with a mixture of sweet thai tea to refresh your day."
-//             },
-//             {
-//                 imageSrc: "src/img/blacktea.jpg",
-//                 title: "Black tea",
-//                 description: "Drink with a blend of robust black tea to invigorate your senses"
-//             },
-//             {
-//                 imageSrc: "src/img/jasminetea.jpg",
-//                 title: "Jasmine Tea",
-//                 description: "Unwind with the delicate aroma of jasmine tea to soothe your soul"
-//             },
-//         ]
+class Menu extends React.Component {
+    render() {
+        const menuItem = [
+            {
+                imageSrc: "src/img/dcream.jpg",
+                title: "D'cream",
+                description: "Drink with a combination of coffee and fresh milk to lift your mood."
+            },
+            {
+                imageSrc: "src/img/greentea.jpg",
+                title: "Green tea",
+                description: "Drink with a mixture of sweet green tea to refresh your day."
+            },
+            {
+                imageSrc: "src/img/thaitea.jpg",
+                title: "Thai tea",
+                description: "Drink with a mixture of sweet thai tea to refresh your day."
+            },
+            {
+                imageSrc: "src/img/blacktea.jpg",
+                title: "Black tea",
+                description: "Drink with a blend of robust black tea to invigorate your senses"
+            },
+            {
+                imageSrc: "src/img/jasminetea.jpg",
+                title: "Jasmine Tea",
+                description: "Unwind with the delicate aroma of jasmine tea to soothe your soul"
+            },
+        ]
 
-//             const navigationNextRef = useRef(null);
-//             const navigationPrevRef = useRef(null);
+            const navigationNextRef = useRef(null);
+            const navigationPrevRef = useRef(null);
 
-//             return (
-//                 <section className="menu flex flex-col items-center justify-center pb-[50px] bg-gradient-to-b from-softbrown to-white">
-//                     <h1 className="flex flex-col items-center text-center tracking-wide text-4xl font-bold text-[#2d2424] ">
-//                         OUR MENU
-//                         <div className="bg-white w-[220px] h-[3px] flex items-center justify-center"></div>
-//                     </h1>
-//                     <div className="menu-items-all mt-[40px] max-w-[900px] flex flex-row justify-center items-center gap-[30px]">
-//                         <Swiper
-//                             modules={[Navigation]}
-//                             navigation={{
-//                                 prevEl: navigationPrevRef.current,
-//                                 nextEl: navigationNextRef.current,
-//                             }}
+            return (
+                <section className="menu flex flex-col items-center justify-center pb-[50px] bg-gradient-to-b from-softbrown to-white">
+                    <h1 className="flex flex-col items-center text-center tracking-wide text-4xl font-bold text-[#2d2424] ">
+                        OUR MENU
+                        <div className="bg-white w-[220px] h-[3px] flex items-center justify-center"></div>
+                    </h1>
+                    <div className="menu-items-all mt-[40px] max-w-[900px] flex flex-row justify-center items-center gap-[30px]">
+                        <Swiper
+                            modules={[Navigation]}
+                            navigation={{
+                                prevEl: navigationPrevRef.current,
+                                nextEl: navigationNextRef.current,
+                            }}
 
-//                             onBeforeInit={(swiper) => {
-//                                 swiper.navigation.nextEl = navigationNextRef.current;
-//                                 swiper.navigation.prevEl = navigationPrevRef.current;
-//                             }}
-//                             spaceBetween={50}
-//                             slidesPerView={3}
-//                             onSlideChange={() => console.log('slide change')}
-//                             onSwiper={(swiper) => console.log(swiper)}
-//                         >
-//                             <button ref={navigationPrevRef} className="swipe-left">
-//                                 <img src="src/img/chevron-left.png" alt="left" className="w-[70px]" />
-//                             </button>
-//                             {menuItem.map((item, index) => (
-//                                 <SwiperSlide key={index}>
-//                                     <div className="item-menu flex flex-col items-center justify-center !w-[250px] !h-[350px] bg-white p-4 rounded-3xl shadow-md mb-[20px]">
-//                                         <div className="image max-h-[220px] overflow-hidden rounded-xl">
-//                                             <img src={item.imageSrc} alt={item.title} className="w-[200px] object-contain" />
-//                                         </div>
-//                                         <div className="description text-justify">
-//                                             <h3 className="mt-[15px] text-[#B85C38] tracking-wide text-xl font-bold">{item.title}</h3>
-//                                             <p className="text-[#2d2424] text-base tracking-wide font-normal">
-//                                                 {item.description}
-//                                             </p>
-//                                         </div>
-//                                     </div>
-//                                 </SwiperSlide>
-//                             ))}
-//                             <button ref={navigationNextRef} className="swipe-right">
-//                                 <img src="src/img/chevron-left.png" alt="right" className="w-[70px] transform rotate-180" />
-//                             </button>
-//                         </Swiper>
-//                     </div>
-//                 </section>
-//             )
-//     }
-// }
+                            onBeforeInit={(swiper) => {
+                                swiper.navigation.nextEl = navigationNextRef.current;
+                                swiper.navigation.prevEl = navigationPrevRef.current;
+                            }}
+                            spaceBetween={50}
+                            slidesPerView={3}
+                            onSlideChange={() => console.log('slide change')}
+                            onSwiper={(swiper) => console.log(swiper)}
+                        >
+                            <button ref={navigationPrevRef} className="swipe-left">
+                                <img src="src/img/chevron-left.png" alt="left" className="w-[70px]" />
+                            </button>
+                            {menuItem.map((item, index) => (
+                                <SwiperSlide key={index}>
+                                    <div className="item-menu flex flex-col items-center justify-center !w-[250px] !h-[350px] bg-glass p-4 rounded-3xl shadow-md mb-[20px]">
+                                        <div className="image max-h-[220px] overflow-hidden rounded-xl">
+                                            <img src={item.imageSrc} alt={item.title} className="w-[200px] object-contain" />
+                                        </div>
+                                        <div className="description text-justify">
+                                            <h3 className="mt-[15px] text-[#B85C38] tracking-wide text-xl font-bold">{item.title}</h3>
+                                            <p className="text-[#2d2424] text-base tracking-wide font-normal">
+                                                {item.description}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                            <button ref={navigationNextRef} className="swipe-right">
+                                <img src="src/img/chevron-left.png" alt="right" className="w-[70px] transform rotate-180" />
+                            </button>
+                        </Swiper>
+                    </div>
+                </section>
+            )
+    }
+}
 
-// export default Menu;
+export default Menu;
--- kopstud-react/src/components/Socmed.jsx
+++ kopstud-react/src/components/Socmed.jsx
@@ -1,53 +1,59 @@
 import React from "react";
-import { FaInstagram } from "react-icons/fa";
 
 class Socmed extends React.Component {
-    render() {
-        const socmedItem = [
-            {
-                title: 'Instagram',
-                icon: '<FaInstagram />',
-                idsocmed: '@Kopistudio20',
-                link: 'https://instagram.com'
-            },
-            {
-                title: 'Facebook',
-                idsocmed: 'Kopi Studio 20',
-                link: ''
-            },
-            {
-                title: 'Twitter',
-                idsocmed: '@kopistudio20',
-                link: ''
-            },
-            {
-                title: 'Email',
-                idsocmed: 'kopistudio20@gmail.com',
-                link: ''
-            },
-        ]
-        return (
-            <section className="flex flex-col items-center justify-center">
-                <div className="w-full text-center bg-[#797160] py-[20px]">
-                    <p className="text-white">Get Up and Close Personal</p>
-                    <h1 className="flex flex-col items-center text-center tracking-wide text-4xl font-bold text-[#2d2424] ">
-                        CONTACT US
-                        <div className="bg-white w-[250px] h-[3px] flex items-center justify-center"></div>
-                    </h1>
-                </div>
-                <div className="grid mt-[40px] grid-cols-4 justify-center">
-                    {socmedItem.map((item, index) => (
-                        <div key={index} className="flex flex-col items-center justify-center mb-[20px] tracking-wide cursor-pointer max-w-[280px] h-[250px] overflow-hidden p-[35px] border-r-3 border-[#797160]">
-                            <FaInstagram />
-                            <h3 className="mt-[20px] text-2xl font-bold">{item.title}</h3>
-                            <p className="text-sm text-chocobrown"><a href={item.link}>{item.idsocmed}</a></p>
-                        </div>
-                    ))
-                    }
-                </div>
-            </section>
-        )
-    }
+  render() {
+    const socmedItem = [
+      {
+        title: "Instagram",
+        icon: "https://img.icons8.com/material-outlined/80/2D2424/instagram-new--v1.png",
+        idsocmed: "@Kopistudio20",
+        link: "https://instagram.com",
+      },
+      {
+        title: "Facebook",
+        icon: "https://img.icons8.com/material-outlined/80/2D2424/facebook.png",
+        idsocmed: "Kopi Studio 20",
+        link: "",
+      },
+      {
+        title: "X",
+        icon: "https://img.icons8.com/material-outlined/80/2D2424/twitterx--v1.png",
+        idsocmed: "@kopistudio20",
+        link: "",
+      },
+      {
+        title: "Email",
+        icon: "https://img.icons8.com/material-outlined/80/2D2424/new-post.png",
+        idsocmed: "kopistudio20@gmail.com",
+        link: "",
+      },
+    ];
+    return (
+      <section className="flex flex-col items-center justify-center">
+        <div className="w-full text-center bg-[#797160] py-[20px]">
+          <p className="text-white">Get Up and Close Personal</p>
+          <h1 className="flex flex-col items-center text-center tracking-wide text-4xl font-bold text-[#2d2424] ">
+            CONTACT US
+            <div className="bg-white w-[250px] h-[3px] flex items-center justify-center"></div>
+          </h1>
+        </div>
+        <div className="grid mt-[40px] grid-cols-4 justify-center">
+          {socmedItem.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center mb-[20px] tracking-wide cursor-pointer max-w-[280px] h-[250px] overflow-hidden p-[35px] border-r-3 border-[#797160]"
+            >
+              <img src={item.icon} alt={item.title} className="color-darkbrown"/>
+              <h3 className="mt-[20px] text-2xl font-bold text-darkbrown">{item.title}</h3>
+              <p className="text-sm text-chocobrown">
+                <a href={item.link}>{item.idsocmed}</a>
+              </p>
+            </div>
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Socmed;
